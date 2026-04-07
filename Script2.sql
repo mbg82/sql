5c02ba3a486e080f135d4d44ad8e176046d1a726
mbg82 <ansqud14@gmail.com>
2026-04-07 18:36:59 +0900

    추가 연습

--- Script2.sql
+++ Script2.sql
@@ -2,6 +2,9 @@ SELECT *FROM student;
 
 SELECT *FROM department;
 
+DELETE FROM professor 
+WHERE name='김태석';
+
 SELECT student.name, student.stu_id, department.dept_name
 FROM student, DEPARTMENT
 WHERE student.dept_id = department.dept_id;
@@ -49,3 +52,50 @@ FROM student s1, student s2;
 SELECT name AS 이름, POSITION 직위, 2026-year_emp 재직연수
 FROM professor
 ORDER BY name;
+
+SELECT *
+FROM STUDENT
+WHERE name LIKE '김%';
+
+SELECT *
+FROM student
+WHERE resident_id LIKE '%*2%' or RESIDENT_ID LIKE '%*4%'
+
+
+SELECT name FROM student
+UNION all
+SELECT name FROM professor;  -- 합집합
+
+SELECT * FROM professor;
+
+SELECT * FROM student;
+
+SELECT stu_id  
+FROM student s, department d
+WHERE s.dept_id=d.dept_id AND dept_name='컴퓨터공학과'
+INTERSECT
+SELECT stu_id
+FROM takes
+WHERE grade='A+';  --교집합
+
+SELECT stu_id  --1292303,1292305
+FROM student s, department d
+WHERE s.dept_id=d.dept_id 
+AND dept_name='산업공학과'
+MINUS
+SELECT stu_id  -- 1292001,1292003,1292303
+FROM takes
+WHERE grade='A+';  -- 차집합
+
+SELECT title, credit, YEAR, semester, division
+FROM course, class
+WHERE course.course_id=class.course_id;
+
+SELECT * FROM course;
+
+SELECT * FROM class;
+
+
+
+
+
